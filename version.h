1503825e0edeec7de8ffa90ee73555b096001e82
BattyBovine <github@battybovine.com>
2015-01-31 19:00:42 -0500

    Version number increased to 0.5.x.x.

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 namespace Version
 {
     static const int MAJOR = 0;
-    static const int MINOR = 2;
+    static const int MINOR = 5;
     static const int REVISION = 18;
     static const int BUILD = 421;
 }
